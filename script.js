64ef5c58515387d787c0315300d062cb10fc701b
eltonccdantas <elton.chagas.dantas@gmail.com>
2024-04-10 19:02:49 -0300

    Create script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+
+// Hamburguer menu
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburguer-icon");
-
+    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
